fcf3ac1ea72012e6b48db30fd1a5553c60d2714b
Hazem <hazem.ismail@hotmail.com>
2026-03-26 19:02:33 +0300

    remove unguarded

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -7,12 +7,12 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Attributes\Appends;
-use Illuminate\Database\Eloquent\Attributes\UnGuarded;
+// use Illuminate\Database\Eloquent\Attributes\UnGuarded;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[UnGuarded]
+// #[UnGuarded]
 // #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 #[Appends(['can', 'name'])]
@@ -20,6 +20,8 @@ class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    protected $guarded = [];
 
     /**
      * Get the attributes that should be cast.
